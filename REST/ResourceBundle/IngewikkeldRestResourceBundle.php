5bbb68604f8204aa0e46fa010dea91c8c349e558
Stefan Koopmanschap <left@leftontheweb.com>
2014-06-25 17:22:41 +0200

    added bundle class

---
+++ REST/ResourceBundle/IngewikkeldRestResourceBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Symfony REST Edition.
+ *
+ * @link      https://github.com/Ingewikkeld/symfony-rest-edition
+ * @copyright Copyright (c) 2013-2014 Ingewikkeld<info@ingewikkeld.net>
+ * @license   https://github.com/Ingewikkeld/symfony-rest-edition/blob/master/LICENSE MIT License
+ */
+
+namespace Ingewikkeld\Rest\ResourceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IngewikkeldRestResourceBundle extends Bundle
+{
+}
